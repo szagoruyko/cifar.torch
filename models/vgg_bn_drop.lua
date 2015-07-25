16568f75f4de1a1446e5b0bda78bf30721a96f04
Sergey Zagoruyko <zagoruyko2@gmail.com>
2015-07-26 01:45:28 +0200

    first commit

---
+++ models/vgg_bn_drop.lua
@@ -0,0 +1,84 @@
+require 'nn'
+require 'cunn'
+
+local backend_name = 'cudnn'
+
+local backend
+if backend_name == 'cudnn' then
+  require 'cudnn'
+  backend = cudnn
+else
+  backend = nn
+end
+  
+local vgg = nn.Sequential()
+
+-- building block
+local function ConvBNReLU(nInputPlane, nOutputPlane)
+  vgg:add(backend.SpatialConvolution(nInputPlane, nOutputPlane, 3,3, 1,1, 1,1))
+  vgg:add(nn.SpatialBatchNormalization(nOutputPlane,1e-3))
+  vgg:add(backend.ReLU(true))
+  return vgg
+end
+
+-- Will use "ceil" MaxPooling because we want to save as much
+-- space as we can
+local MaxPooling = backend.SpatialMaxPooling
+
+ConvBNReLU(3,64):add(nn.Dropout(0.3))
+ConvBNReLU(64,64)
+vgg:add(MaxPooling(2,2,2,2):ceil())
+
+ConvBNReLU(64,128):add(nn.Dropout(0.4))
+ConvBNReLU(128,128)
+vgg:add(MaxPooling(2,2,2,2):ceil())
+
+ConvBNReLU(128,256):add(nn.Dropout(0.4))
+ConvBNReLU(256,256):add(nn.Dropout(0.4))
+ConvBNReLU(256,256)
+vgg:add(MaxPooling(2,2,2,2):ceil())
+
+ConvBNReLU(256,512):add(nn.Dropout(0.4))
+ConvBNReLU(512,512):add(nn.Dropout(0.4))
+ConvBNReLU(512,512)
+vgg:add(MaxPooling(2,2,2,2):ceil())
+
+-- In the last block of convolutions the inputs are smaller than
+-- the kernels and cudnn doesn't handle that, have to use cunn
+backend = nn
+ConvBNReLU(512,512):add(nn.Dropout(0.4))
+ConvBNReLU(512,512):add(nn.Dropout(0.4))
+ConvBNReLU(512,512)
+vgg:add(MaxPooling(2,2,2,2):ceil())
+vgg:add(nn.View(512))
+
+classifier = nn.Sequential()
+classifier:add(nn.Dropout(0.5))
+classifier:add(nn.Linear(512,512))
+classifier:add(nn.BatchNormalization(512))
+classifier:add(nn.ReLU(true))
+classifier:add(nn.Dropout(0.5))
+classifier:add(nn.Linear(512,10))
+vgg:add(classifier)
+
+-- initialization from MSR
+local function MSRinit(net)
+  local function init(name)
+    for k,v in pairs(net:findModules(name)) do
+      local n = v.kW*v.kH*v.nOutputPlane
+      v.weight:normal(0,math.sqrt(2/n))
+      v.bias:zero()
+    end
+  end
+  -- have to do for both backends
+  init'cudnn.SpatialConvolution'
+  init'nn.SpatialConvolution'
+end
+
+MSRinit(vgg)
+
+-- check that we can propagate forward without errors
+-- should get 16x10 tensor
+--print(#vgg:cuda():forward(torch.CudaTensor(16,3,32,32)))
+
+return vgg
---
+++ train.lua
@@ -0,0 +1,195 @@
+require 'xlua'
+require 'optim'
+require 'cutorch'
+dofile './provider.lua'
+local c = require 'trepl.colorize'
+
+opt = lapp[[
+   -s,--save                  (default "logs")      subdirectory to save logs
+   -b,--batchSize             (default 128)          batch size
+   -r,--learningRate          (default 1)        learning rate
+   --learningRateDecay        (default 1e-7)      learning rate decay
+   --weightDecay              (default 0.0005)      weightDecay
+   -m,--momentum              (default 0.9)         momentum
+   --epoch_step               (default 25)          epoch step
+   --model                    (default vgg16bn)     model name
+]]
+
+print(opt)
+
+do -- data augmentation module
+  local BatchFlip,parent = torch.class('nn.BatchFlip', 'nn.Module')
+
+  function BatchFlip:__init()
+    parent.__init(self)
+    self.train = true
+  end
+
+  function BatchFlip:updateOutput(input)
+    if self.train then
+      local bs = input:size(1)
+      local flip_mask = torch.randperm(bs):le(bs/2)
+      for i=1,input:size(1) do
+        if flip_mask[i] == 1 then image.hflip(input[i], input[i]) end
+      end
+    end
+    self.output = input
+    return self.output
+  end
+end
+
+print(c.blue '==>' ..' configuring model')
+local model = nn.Sequential()
+model:add(nn.BatchFlip():float())
+model:add(nn.Copy('torch.FloatTensor','torch.CudaTensor'):cuda())
+model:add(nn.Sequential():add(dofile('models/'..opt.model..'.lua'):cuda()))
+model:get(2).updateGradInput = function(input) return end
+print(model)
+
+print(c.blue '==>' ..' loading data')
+provider = torch.load 'provider.t7'
+
+confusion = optim.ConfusionMatrix(10)
+
+print('Will save at '..opt.save)
+paths.mkdir(opt.save)
+testLogger = optim.Logger(paths.concat(opt.save, 'test.log'))
+testLogger:setNames{'% mean class accuracy (train set)', '% mean class accuracy (test set)'}
+
+parameters,gradParameters = model:getParameters()
+
+
+print(c.blue'==>' ..' setting criterion')
+criterion = nn.CrossEntropyCriterion():cuda()
+
+
+print(c.blue'==>' ..' configuring optimizer')
+optimState = {
+  learningRate = opt.learningRate,
+  weightDecay = opt.weightDecay,
+  momentum = opt.momentum,
+  learningRateDecay = opt.learningRateDecay,
+}
+optimMethod = optim.sgd
+
+
+function train()
+  model:training()
+  epoch = epoch or 1
+
+  -- drop learning rate every "epoch_step" epochs
+  if epoch % opt.epoch_step == 0 then optimState.learningRate = optimState.learningRate/2 end
+  
+  print(c.blue '==>'.." online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ']')
+
+  local targets = torch.CudaTensor(opt.batchSize)
+  local indices = torch.randperm(provider.trainData.data:size(1)):long():split(opt.batchSize)
+  indices[#indices] = nil
+
+  local tic = torch.tic()
+  for t,v in ipairs(indices) do
+    xlua.progress(t, #indices)
+
+    inputs = provider.trainData.data:index(1,v)
+    targets:copy(provider.trainData.labels:index(1,v))
+
+    local feval = function(x)
+      if x ~= parameters then parameters:copy(x) end
+      gradParameters:zero()
+      
+      local outputs = model:forward(inputs)
+      local f = criterion:forward(outputs, targets)
+      local df_do = criterion:backward(outputs, targets)
+      model:backward(inputs, df_do)
+
+      confusion:batchAdd(outputs, targets)
+
+      return f,gradParameters
+    end
+    optim.sgd(feval, parameters, optimState)
+  end
+
+  confusion:updateValids()
+  print(('Train accuracy: '..c.cyan'%.2f'..' %%\t time: %.2f s'):format(
+        confusion.totalValid * 100, torch.toc(tic)))
+
+  train_acc = confusion.totalValid * 100
+
+  confusion:zero()
+  epoch = epoch + 1
+end
+
+
+function test()
+  model:training()
+  print(c.blue '==>'.." doing some iterations on train data for batch normalization")
+  do
+    local indices = torch.randperm(provider.trainData.data:size(1)):long():split(opt.batchSize)
+    for i=101,#indices do indices[i] = nil end
+
+    for i,v in ipairs(indices) do
+      model:forward(provider.trainData.data:index(1,v))
+    end
+  end
+
+  -- disable flips, dropouts and batch normalization
+  model:evaluate()
+  print(c.blue '==>'.." testing")
+  local n = provider.testData.data:size(1)
+  local bs = 125
+  for i=1,n,bs do
+    _,y = model:forward(provider.testData.data:narrow(1,i,bs)):max(2)
+    confusion:batchAdd(y,provider.testData.labels:narrow(1,i,bs))
+  end
+
+  confusion:updateValids()
+  print('Test accuracy:', confusion.totalValid * 100)
+  
+  if testLogger then
+    paths.mkdir(opt.save)
+    testLogger:add{train_acc, confusion.totalValid * 100}
+    --testLogger:style{'-','-'}
+    --testLogger:plot()
+    local file = io.open(opt.save..'/report.html','w')
+    local header = [[
+    <!DOCTYPE html>
+    <html>
+    <body>
+    <title>$SAVE - $EPOCH</title>
+    <img src="test.png">
+    ]]
+    header = header:gsub('$SAVE',opt.save):gsub('$EPOCH',epoch)
+    file:write(header)
+    file:write'<h4>optimState:</h4>\n'
+    file:write'<table>\n'
+    for k,v in pairs(optimState) do
+      if torch.type(v) == 'number' then
+        file:write('<tr><td>'..k..'</td><td>'..v..'</td></tr>\n')
+      end
+    end
+    file:write'</table><pre>\n'
+    file:write(tostring(confusion)..'\n')
+    file:write(tostring(model)..'\n')
+    file:write("</pre></body></html>")
+    file:close()
+    os.execute('convert -density 200 '..opt.save..'/test.log.eps '..opt.save..'/test.png')
+  end
+  --[[
+  do
+    local filename = paths.concat(opt.save, 'model.net')
+    print('==> saving model to '..filename)
+    torch.save(filename, model:get(3))
+  end
+  ]]--
+
+  confusion:zero()
+end
+
+
+--for i=1,250 do
+while true do
+  train()
+  test()
+end
+
+
