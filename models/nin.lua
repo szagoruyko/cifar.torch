d9e9cdf34e41824486749b1345ee40f78f2f2817
Sergey Zagoruyko <zagoruyko2@gmail.com>
2016-01-20 15:30:13 +0100

    cudnn.convert

--- models/nin.lua
+++ models/nin.lua
@@ -3,40 +3,30 @@ require 'nn'
 -- Network-in-Network
 -- achieves 92% with BN and 88% without
 
-local backend_name = 'cudnn'
-
-local backend
-if backend_name == 'cudnn' then
-  require 'cudnn'
-  backend = cudnn
-else
-  backend = nn
-end
-  
 local model = nn.Sequential()
 
 local function Block(...)
   local arg = {...}
-  model:add(backend.SpatialConvolution(...))
+  model:add(nn.SpatialConvolution(...))
   model:add(nn.SpatialBatchNormalization(arg[2],1e-3))
-  model:add(backend.ReLU(true))
+  model:add(nn.ReLU(true))
   return model
 end
 
 Block(3,192,5,5,1,1,2,2)
 Block(192,160,1,1)
 Block(160,96,1,1)
-model:add(backend.SpatialMaxPooling(3,3,2,2):ceil())
+model:add(nn.SpatialMaxPooling(3,3,2,2):ceil())
 model:add(nn.Dropout())
 Block(96,192,5,5,1,1,2,2)
 Block(192,192,1,1)
 Block(192,192,1,1)
-model:add(backend.SpatialAveragePooling(3,3,2,2):ceil())
+model:add(nn.SpatialAveragePooling(3,3,2,2):ceil())
 model:add(nn.Dropout())
 Block(192,192,3,3,1,1,1,1)
 Block(192,192,1,1)
 Block(192,10,1,1)
-model:add(backend.SpatialAveragePooling(8,8,1,1):ceil())
+model:add(nn.SpatialAveragePooling(8,8,1,1):ceil())
 model:add(nn.View(10))
 
 for k,v in pairs(model:findModules(('%s.SpatialConvolution'):format(backend_name))) do
